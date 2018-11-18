97db8803760e0766fc852cb604cfd8c892a1749c
yashima ryota <fukuoka19_1@icloud.com>
2018-11-18 22:15:50 +0900

    movetest.py

---
+++ movetest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from core_tool import *
+def Run(ct,*args):
+    # 初期位置移動
+    q0 = [0]*7
+    q0[0] = -0.022254568871029695
+    q0[1] = 0.027604284836455075
+    q0[2] = 0.022568074910412333
+    q0[3] = -2.200118621520332
+    q0[4] = -0.00047771840080773566
+    q0[5] = 0.656946868009727
+    q0[6] = 0.0010118998269884236
+    ct.robot.MoveToQ(q0, 3.0, blocking = True)
+    # 初期位置固定
+    x = list(ct.robot.FK())
+    x0 = copy.deepcopy(x)
+    # 腕を伸ばす
+    x1 = copy.deepcopy(x0)
+    x1[0] += 0.3
+    ct.robot.MoveToX(x1, 3.0, blocking = True)
+    # 掴む
+    ct.robot.OpenGripper()
+    ct.robot.MoveGripper(0.1)
+    # 初期位置に戻る
+    ct.robot.MoveToX(x0, 3.0, blocking = True)
+    # 離す
+    ct.robot.OpenGripper()
